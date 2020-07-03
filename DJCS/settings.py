72a6854314d9117fadd3ff28e6c972ba6ba4a7b7
abner barrios hernandez <borita_2805@hotmail.com>
2020-07-03 16:53:21 -0500

    prueba 1

--- DJCS/settings.py
+++ DJCS/settings.py
@@ -37,9 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+
+    'rest_framework',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    #apps
+    'apps.endpoints',
 ]
 SITE_ID = 1
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
- web: gunicorn DJCS.wsgi:application --pythonpath app --log-file -
+web:gunicorn DJCS.wsgi:application --pythonpath app --log-file -
