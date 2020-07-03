b09f967860ad614246a73933a2ba82197c8f56a8
abner barrios hernandez <borita_2805@hotmail.com>
2020-07-02 21:49:49 -0500

    add secret

--- DJCS/settings.py
+++ DJCS/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#weq_6&p^4quv1e8yb@xdw3cij7l^b@yrm#b5x3nm$g6ghu0ku'
 
+SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
